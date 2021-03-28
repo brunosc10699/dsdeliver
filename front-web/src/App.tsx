292088ec0ab438bbb36850862bb0f9ec587f3d38
brunosc10699 <brunosc10699@gmail.com>
2021-03-27 22:06:55 -0300

    Home page added

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
