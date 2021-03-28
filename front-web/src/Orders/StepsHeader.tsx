aee9ed1f6357618dff1fd487c0a703f380086699
brunosc10699 <brunosc10699@gmail.com>
2021-03-28 19:26:01 -0300

    Routes added

---
+++ front-web/src/Orders/StepsHeader.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function StepsHeader(){
+    return (
+        <header className="order-steps-container">
+            
+        </header>
+    );
+}
+
+export default StepsHeader;
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Orders() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Orders;
