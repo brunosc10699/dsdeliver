a4e4af7a32b4ea1da6e9b365496c5e6abc83506c
brunosc10699 <brunosc10699@gmail.com>
2021-07-27 20:24:23 -0300

    fix total

--- backend/src/main/java/com/bruno/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/bruno/dsdeliver/entities/Order.java
@@ -97,6 +97,14 @@ public class Order implements Serializable {
 	public OrderStatus getStatus() {
 		return status;
 	}
+	
+	public Double getTotal() {
+		double sum = 0d;
+		for(Product p : products) {
+			sum += p.getPrice();
+		}
+		return sum;
+	}
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
